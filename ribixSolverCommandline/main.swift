6c9593a90bb2d4bf604a9078cb5dbc97c2ae1616
Sea-Snell <ChunkDaSnail@gmail.com>
2015-07-03 13:36:22 -0500

    Initial Commit

---
+++ ribixSolverCommandline/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ribixSolverCommandline
+//
+//  Created by Charlie Snell on 7/3/15.
+//  Copyright (c) 2015 sea_software. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
